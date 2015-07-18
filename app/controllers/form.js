ff4347ded6ae8422e993fe02c9b19f916795b3a2
egregori <egregoriblasco@gmail.com>
2015-07-18 11:12:04 +0200

    Documentación de codigo

--- app/controllers/form.js
+++ app/controllers/form.js
@@ -1,39 +1,12 @@
 /**
- *                              _                _             
- *                             | |              | |            
- *    __ _ _ __  _ __   ___ ___| | ___ _ __ __ _| |_ ___  _ __ 
- *   / _` | '_ \| '_ \ / __/ _ \ |/ _ \ '__/ _` | __/ _ \| '__|
- *  | (_| | |_) | |_) | (_|  __/ |  __/ | | (_| | || (_) | |   
- *   \__,_| .__/| .__/ \___\___|_|\___|_|  \__,_|\__\___/|_|   
- *        | |   | |                                            
- *        |_|   |_|  
- *      
- *      
- * @overview
- * This is the controller file for the Directory View. The directory view loads data from 
- * a flat file, and derives a Sectioned and Indexed (iOS) ListView displaying all contacts.
- * The Directory has two ListView Templates, one for standard contacts, the other to denote
- * that you have a marked the contact as a Bookmark (or Favorite). Also, the Directory View
- * can be filtered so that it only displays bookmarked or favorited contacts.
- *
- * @copyright
- * Copyright (c) 2014 by Appcelerator, Inc. All Rights Reserved.
- *
- * @license
- * Licensed under the terms of the Apache Public License
- * Please see the LICENSE included with this distribution for details.
+ * Instanciamos las variables locales para este controlador
  */
-
+var _args = arguments[0] || {}, //Obtenemos cualquier argumento que se haya pasado al generar la instancia del controlador
+	App = Alloy.Globals.App, //Referencia al objeto singleton de APP
+	indexes = [];  // Necesario para iOS para algunos elementos como listView o listItem
+	
 /**
- * Instantiate the local variables for this controller
- */
-var _args = arguments[0] || {}, // Any passed in arguments will fall into this property
-	App = Alloy.Globals.App, // reference to the APP singleton object
-	users = null,  // Array placeholder for all users
-	indexes = [];  // Array placeholder for the ListView Index (used by iOS only);
-
-/**
- * Appcelerator Analytics Call
+ * Generamos el titulo de la pantalla y iniciamos servicio de analitica  y mandamos un page view
  */
 var title = _args.title ? _args.title.toLowerCase() : "HADA";
 Ti.Analytics.featureEvent(Ti.Platform.osname+"."+title+".viewed");
@@ -226,25 +199,32 @@ var questions = [{
  */
 function init(){
 	
-	
+	//Recorremos cada grupo de preguntas con sus preguntas y propiedades
 	_.each(questions, function(groupQuestionObject, id){
 		
+		//Para cada grupo generamos un contenedor
 		var questionGroupContainerView = Ti.UI.createView({
 			layout: 'horizontal',
 			height: Ti.UI.SIZE,
 		});
 		
 		$.wrapper.add(questionGroupContainerView);
-		
+		//Generamos la cabecera del grupo de preguntas que mostrara el texto de cada grupo de preguntas
 		generateGroupHeader(questionGroupContainerView, groupQuestionObject, id);
+		
+		//Generamos el cuerpo del grupo de preguntas. Cada grupo de preguntas se compone de un label y de botones o inputs
 		generateGroupQuestions(questionGroupContainerView, groupQuestionObject, id);
 		
 	});
 	
+	//Inyectamos el boton de enviar
 	generateSendButton();
 	
 };
 
+/**
+ * Este metodo se encarga del renderizado del boton de enviar y de añadir un evento al mismo para que se envie el formulario 
+ */
 function generateSendButton(){
 		
 
@@ -266,16 +246,19 @@ function generateSendButton(){
 				
 		$.wrapper.add(buttonSubmit);
 		
+		//Attach de evento click sobre el boton. Cuando se haga un click se invocara al metodo sendForm encargado de enviar el formulario
 		buttonSubmit.addEventListener("click", sendForm);
 }
 
-
+/**
+ * Metodo encargado de enviar los datos del formulatio a la pagina. 
+ */
 function sendForm(){
 	var httpClient = Titanium.Network.createHTTPClient();
 	httpClient.open('POST', 'http://emac.uv.es/HADA/calculo_ajax.php');
-  	httpClient.send(questionsResponses);
+  	httpClient.send(questionsResponses); //Enviamos el objeto que ya contiene la estructura necesaria.
   	httpClient.onload = function(){
-  		 //check the response code returned
+  		 //TODO: Checkear diferentes estados
 	    if(this.status == 200)
 	    {
 	        checkResponse(this.responseText);	        
@@ -284,10 +267,15 @@ function sendForm(){
 	
 };
 
+/**
+ * Metodo encargado de procesar la respuesta del servidor al formulario enviado y mostrar el resultado. 
+ */
 function checkResponse(data){
 	/** BUSCAMOS CUALQUIER CONTENIDO EN UN ELEMENTO SPAN (SOLO EXISTE UNO EN LA RESPUESTA DE LA LLAMADA )**/
 	var patt = "<span(?:[^>]+class=\"(.*?)\"[^>]*)?>(.*?)<\/span>";
 	var res = data.match(patt);
+	
+	/**TODO: Pantalla de resultado utilizando el navigation window **/
 	alert("El resultado es: "+res[2]);
 };
 
@@ -297,7 +285,8 @@ function checkResponse(data){
  * @param id {Int} identificador unico del fieldset
  */
 function generateGroupHeader(questionGroupView, groupQuestionObject, id){
-	
+		
+		//Generamos el contenedor del label y lo añadimos a su questionGroup
 		var groupHeaderView = Ti.UI.createView({
 			backgroundColor: "#ececec",
 			width: '100%',
@@ -307,6 +296,7 @@ function generateGroupHeader(questionGroupView, groupQuestionObject, id){
 		
 		questionGroupView.add(groupHeaderView);
 		
+		//Generamos el label que contiene el titulo del grupo de preguntas
 		var groupHeaderLabel = Ti.UI.createLabel({
 			left: 20,
 		 	font:{
@@ -321,7 +311,9 @@ function generateGroupHeader(questionGroupView, groupQuestionObject, id){
 }
 
 
-
+/**
+ * Se llama cada vez que se interactua con un field. Este metodo se encarga de añadir respuestas a un hash donde la key es el name del field y el value el valor del field en el formulatio
+ */
 function processQuestionResponse(name, value){
 	
 	questionsResponses[name] = value;
@@ -329,18 +321,18 @@ function processQuestionResponse(name, value){
 }
 
 /**
- * Se ejecuta al hacer un click sobre qualquier botón o al introducir texto en un textField 
+ * Se ejecuta al hacer un click sobre qualquier botón del formulario
  */
 function buttonPressed(e, button){
 	
-	_.each(e.source.parent.children, function(children){
-		if(e.source.id == children.id){
+	_.each(e.source.parent.children, function(children){ //recorremos todos los hijos del padre del boton presionado
+		if(e.source.id == children.id){ //si un hijo contiene el mismo id que el del boton presionado, cambiamos su estilo y procesamos su respuesta llamando a processQuestionResponse
 			children.setBackgroundColor("#CCC");
 			children.setBorderColor('white');
 			children.setColor("#FFF");
 			processQuestionResponse(button.name, e.source.value);
 		}
-		else{
+		else{ //Reseteamos el estilo del boton si el hijo no tiene el mismo id que el boton presionado
 			children.setBackgroundColor('#FFF');
 			children.setBorderColor('#336699');
 			children.setColor('#CCC');	
@@ -350,12 +342,19 @@ function buttonPressed(e, button){
 		
 }
 
-
+/**
+ * @param container {Object} Contenedor del boton a generar
+ * @param field {Object} Objeto field del objeto questions
+ * @param groupId {Int} Identificador unico del grupo de preguntas
+ * Metodo que genera los field 
+ */
 function generateFieldsQuestion(container, field, groupId, questionId){
+		
+		//Cada boton es un element de titanium diferente
 		if( field.type == 'select'){
 			var fieldWidth = '40%';
 			if(field.options.length==3)
-				fieldWidth = '25%';
+				fieldWidth = '25%'; //en caso de ser select y tener 3 opciones bajamos el with
 				
 			_.each(field.options, function(option, optionId){
 				var button = Ti.UI.createButton({
@@ -368,7 +367,7 @@ function generateFieldsQuestion(container, field, groupId, questionId){
 					height: 'auto',
 					left: 5,
 					classes: ['Button'],
-					id: groupId+'_'+questionId+'_'+optionId,
+					id: groupId+'_'+questionId+'_'+optionId, //identificador unico formado por el identificador del grupo de preguntas, el identificador de la pregunta y el identificador del boton
 					right: 5,
 					top: 5,
 					name: field.name,
@@ -379,7 +378,7 @@ function generateFieldsQuestion(container, field, groupId, questionId){
 					title: option.label
 				});
 				
-				button.addEventListener("click", function(e){
+				button.addEventListener("click", function(e){ //Guardamos la respuesta en cada interaccion con el boton y generamos estilos nuevos para mostrar como activo / inactivo
 					buttonPressed(e, button);
 				});
 				
@@ -393,14 +392,14 @@ function generateFieldsQuestion(container, field, groupId, questionId){
 		if( field.type == 'textArea'){
 			var fieldText = Ti.UI.createTextArea({
 				hintText: 'Introduzca aquí sus comentarios',
-				width: Ti.UI.FILL,
+				width: Ti.UI.FILL, //Mediante Ti.UI.FILL indicamos que el elemento debe ocupar todo el ancho disponible.
 				height: 80,
 			});
 			
 			container.add(fieldText);
 
 			
-			fieldText.addEventListener("change", function(e){
+			fieldText.addEventListener("change", function(e){ //Guardamos la respuesta en cada interaccion con el text
 				processQuestionResponse(field.name, e.source.value);
 			});
 			
@@ -409,9 +408,9 @@ function generateFieldsQuestion(container, field, groupId, questionId){
 		
 		if( field.type == 'text'){
 			var fieldTextArea = Ti.UI.createTextField({
-				hintText: 'test',
+				hintText: 'test', //Placeholder con el texto de ejemplo, podria  venir dado mediante una propiedade del field
 				top: 7,
-				keyboardType: "Titanium.UI.KEYBOARD_NUMBER_PAD",
+				keyboardType: "Titanium.UI.KEYBOARD_NUMBER_PAD", //Indicamos que el layout del teclado ha de ser numerico.
 				color:"#336699"
 				
 			});
@@ -425,10 +424,17 @@ function generateFieldsQuestion(container, field, groupId, questionId){
 		
 }
 
+/**
+ * @param questionGroupView {Object} Es el contenedor del grupo de preguntas
+ * @param groupQuestionObject {Object} Es el objeto que contiene las preguntas y sus propiedades
+ * @param groupId {Int} Es el identificador unico del grupo de preguntas
+ */
 function generateGroupQuestions(questionGroupView, groupQuestionObject, groupId){
-			
+		
+		//Recorremos las preguntas del grupo de preguntas y generamos su cuerpo
 		_.each(groupQuestionObject.fields, function(question, questionId){
 			
+			//Es el contenedor de cada pregunta
 			var viewContainer = Ti.UI.createView({
 				width: '100%',
 				top: 5,
@@ -439,21 +445,23 @@ function generateGroupQuestions(questionGroupView, groupQuestionObject, groupId)
 				
 			questionGroupView.add(viewContainer);
 
+			//Si es un textarea ocpara todo el ancho
 			if(question.type == "textArea"){
 				var viewLabelContainerWidth = '0%';
 				var viewButtonContainerWidth = '100%';
-			}else{
+			}else{ //de lo contrario simplemente emulamos un float left
 				var viewLabelContainerWidth = '40%';
 				var viewButtonContainerWidth = '60%';
 			}
-				
+			
+			//Contenedor del label de la pregunta que se inyectara en el contenedor de la pregunta
 			var viewLabelContainer = Ti.UI.createView({
 				width: viewLabelContainerWidth,
 			});
 			
 			viewContainer.add(viewLabelContainer);
 			
-		
+			//Contenedor de los botones o inputs tipo text
 			var viewButtonContainer = Ti.UI.createView({
 				width: viewButtonContainerWidth,
 				background: 'red',
@@ -462,6 +470,7 @@ function generateGroupQuestions(questionGroupView, groupQuestionObject, groupId)
 			
 			viewContainer.add(viewButtonContainer);
 			
+			//Label que presenta el nombre de la pregunta
 			var questionLabel =  Ti.UI.createLabel({
 				left: 5,
 				classes: ['title'],
@@ -471,6 +480,7 @@ function generateGroupQuestions(questionGroupView, groupQuestionObject, groupId)
 			
 			viewLabelContainer.add(questionLabel);
 			
+			//Incluimos un separador de 1dpi al final de cada pregunta  
 			var questionSeparator = Ti.UI.createView({
 				backgroundColor: "#ececec",
 				width: Ti.UI.FILL,
@@ -480,6 +490,7 @@ function generateGroupQuestions(questionGroupView, groupQuestionObject, groupId)
 			
 			questionGroupView.add(questionSeparator);
 			
+			//Generamos los botones o inputs para cada pregunta
 			generateFieldsQuestion(viewButtonContainer, question, groupId, questionId);
 							
 		});
@@ -487,6 +498,6 @@ function generateGroupQuestions(questionGroupView, groupQuestionObject, groupId)
 }
 
 /**
- * Initialize View
+ * Llamamos a nuestro construct y generamos el formulario
  */
 init();
