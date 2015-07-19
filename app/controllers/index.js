77b37dd2e7c0d3b1ddb97fd6c05cedef073be6c3
egregori <egregoriblasco@gmail.com>
2015-07-19 13:11:07 +0200

    Nuevos iconos y splash para ios , tzen, android y mobile web
    -Nuevos iconos y splah
    - Documentado controlador index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,25 +1,33 @@
 /**
- * Global Navigation Handler
+ * Manejador de navegacion global
  */
 Alloy.Globals.Navigator = {
 	
 	/**
-	 * Handle to the Navigation Controller
+	 * Manejador de navegacion de la aplicacion
 	 */
 	navGroup: $.nav,
 	
+	/**
+	 * Funcion encargada de abrir una nueva pantalla. En ella se maneja la navegacion para todos los sistemas
+	 * @param {Object} controller
+	 * @param {Object} payload
+	 */
 	open: function(controller, payload){
+		
+		//Obtenemos la vista del controlador a mostrar
 		var win = Alloy.createController(controller, payload || {}).getView();
 		
+		//En ios se utiliza el tabgroup y el metodo openWindow
 		if(OS_IOS){
 			$.nav.openWindow(win);
 		}
 		else if(OS_MOBILEWEB){
-			$.nav.open(win);
+			$.nav.open(win); //En caso de utilizar mobileweb se usaria el meotodo open
 		}
-		else {
+		else { //En caso de ser Android tenemos que preparar el action bar (Android utiliza un sistema de navegacion mediante el action bar (Situado en el top de la ventana)
 			
-			// added this property to the payload to know if the window is a child
+			// Se añade esta propiedad para saber si el window a abrir es una ventana hija
 			if (payload.displayHomeAsUp){
 				
 				win.addEventListener('open',function(evt){
@@ -36,12 +44,12 @@ Alloy.Globals.Navigator = {
 };
 
 
-if(OS_IOS){
+if(OS_IOS){ //En caso de ser ios utilizamos el meotodo open ya que para la nevagacion se utiliza un tabgroup
 	$.nav.open()
 }
-else if(OS_MOBILEWEB){
+else if(OS_MOBILEWEB){ //Si utilizaramos mobile web, primero capturamos la ventana con $.index y luego llamamos al metodo open de titanium
 	$.index.open();
 }
 else{
-	$.index.getView().open();
+	$.index.getView().open(); //En caso de android capturamos la vista actual mediante $.index.getView y llamamos al metodo open de titanium
 } 
